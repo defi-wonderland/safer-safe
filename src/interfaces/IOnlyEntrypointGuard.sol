5a822c44aef2e625d1ac49df548275247feb1a2a
0xJabberwock <0xjabberwock@defi.sucks>
2025-05-08 12:55:32 -0300

    docs: revise NatSpec of OnlyEntrypointGuard

--- src/interfaces/IOnlyEntrypointGuard.sol
+++ src/interfaces/IOnlyEntrypointGuard.sol
@@ -1,22 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.29;
 
+import {ITransactionGuard} from '@safe-smart-account/base/GuardManager.sol';
+
 /**
  * @title IOnlyEntrypointGuard
  * @notice Interface for the OnlyEntrypointGuard contract
  */
-interface IOnlyEntrypointGuard {
+interface IOnlyEntrypointGuard is ITransactionGuard {
   // ~~~ STORAGE METHODS ~~~
 
   /**
-   * @notice Gets the signature type constant for pre-approved hash signatures
-   * @return _preValidatedSignatureType The signature type constant for pre-approved hash signatures
+   * @notice Gets the signature type constant for approved hash signatures
+   * @return _approvedHashSignatureType The signature type constant for approved hash signatures
    */
-  function PRE_VALIDATED_SIGNATURE_TYPE() external view returns (uint256 _preValidatedSignatureType);
+  function APPROVED_HASH_SIGNATURE_TYPE() external view returns (uint256 _approvedHashSignatureType);
 
   /**
-   * @notice Gets the address of the Safe Entrypoint contract
-   * @return _entrypoint The address of the Safe Entrypoint contract
+   * @notice Gets the address of the SafeEntrypoint contract
+   * @return _entrypoint The address of the SafeEntrypoint contract
    */
   function ENTRYPOINT() external view returns (address _entrypoint);
 
@@ -35,9 +37,10 @@ interface IOnlyEntrypointGuard {
   // ~~~ ERRORS ~~~
 
   /**
-   * @notice Thrown when the signature format is invalid
+   * @notice Thrown when a transaction is attempted by an unauthorized sender
+   * @param _sender The unauthorized sender address
    */
-  error InvalidSignatureFormat();
+  error UnauthorizedSender(address _sender);
 
   /**
    * @notice Thrown when a delegate call is attempted to an unauthorized address
@@ -46,18 +49,7 @@ interface IOnlyEntrypointGuard {
   error UnauthorizedDelegateCall(address _target);
 
   /**
-   * @notice Thrown when a transaction is attempted by an unauthorized sender
-   * @param _sender The unauthorized sender address
+   * @notice Thrown when the signature format is invalid
    */
-  error UnauthorizedSender(address _sender);
-
-  /**
-   * @notice Thrown when the caller is not the entrypoint
-   */
-  error CallerNotEntrypoint();
-
-  /**
-   * @notice Thrown when the signer count is insufficient
-   */
-  error InsufficientSigners();
+  error InvalidSignatureFormat();
 }
