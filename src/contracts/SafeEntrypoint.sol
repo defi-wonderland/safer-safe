8f7d0ab16d871e0db3861d1d7924138564c31ca4
0xAustrian <114922365+0xAustrian@users.noreply.github.com>
2025-09-02 12:18:03 -0300

    feat: move constructor checks

--- src/contracts/SafeEntrypoint.sol
+++ src/contracts/SafeEntrypoint.sol
@@ -80,18 +80,18 @@ contract SafeEntrypoint is SafeManageable, OnlyEntrypointGuard, EmergencyModeHoo
     address _emergencyTrigger,
     address _emergencyCaller
   ) SafeManageable(_safe) EmergencyModeHook(_emergencyTrigger, _emergencyCaller) {
+    if (_txExpiryDelay < MIN_EXPIRY_TIME) revert TxExpiryDelayCannotBeLessThanMin();
+    if (_maxApprovalDuration < MIN_EXPIRY_TIME) revert MaxApprovalDurationCannotBeLessThanMin();
+    if (_shortTxExecutionDelay > _longTxExecutionDelay) revert ShortDelayCannotBeGreaterThanLongDelay();
+    if (_txExpiryDelay > type(uint128).max) revert TxExpiryDelayCannotBeGreaterThanMax();
+    if (_longTxExecutionDelay > type(uint128).max) revert LongDelayCannotBeGreaterThanMax();
+
     MULTI_SEND_CALL_ONLY = _multiSendCallOnly;
 
     SHORT_TX_EXECUTION_DELAY = _shortTxExecutionDelay;
     LONG_TX_EXECUTION_DELAY = _longTxExecutionDelay;
     TX_EXPIRY_DELAY = _txExpiryDelay;
     MAX_APPROVAL_DURATION = _maxApprovalDuration;
-
-    if (_txExpiryDelay < MIN_EXPIRY_TIME) revert TxExpiryDelayCannotBeLessThanMin();
-    if (_maxApprovalDuration < MIN_EXPIRY_TIME) revert MaxApprovalDurationCannotBeLessThanMin();
-    if (_shortTxExecutionDelay > _longTxExecutionDelay) revert ShortDelayCannotBeGreaterThanLongDelay();
-    if (_txExpiryDelay > type(uint128).max) revert TxExpiryDelayCannotBeGreaterThanMax();
-    if (_longTxExecutionDelay > type(uint128).max) revert LongDelayCannotBeGreaterThanMax();
   }
 
   // ~~~ ADMIN METHODS ~~~
