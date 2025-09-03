8d4b06fa43b888bddea01dff5001f177247be7dd
itadaki <itadaki@wonderland.xyz>
2025-09-03 18:08:38 +0200

    test: adds missing test

--- test/unit/factories/SimpleTransfersFactory.t.sol
+++ test/unit/factories/SimpleTransfersFactory.t.sol
@@ -35,6 +35,9 @@ contract UnitSimpleTransfersFactory is Test {
     assertEq(_actions[1].target, _transferActionB.token);
     assertEq(_actions[1].data, abi.encodeCall(IERC20.transfer, (_transferActionB.to, _transferActionB.amount)));
     assertEq(_actions[1].value, 0);
+
+    // it should store the contract in the factory
+    assertTrue(simpleTransfersFactory.isChild(_simpleTransfers));
   }
 
   function test_CreateSimpleTransferWhenCreatingASimpleTransfersContractWithASingleTransferAction(
