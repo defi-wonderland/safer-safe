5b0de3c1b922e5f4a7367d233255f1259474130f
itadaki <itadaki@wonderland.xyz>
2025-09-04 18:22:10 +0200

    test: improves tests

--- test/unit/mocks/ActionBuildForTest.sol
+++ test/unit/mocks/ActionBuildForTest.sol
@@ -6,7 +6,5 @@ import {ActionBuilder} from 'src/contracts/actions-builders/ActionBuilder.sol';
 contract ActionBuilderForTest is ActionBuilder {
   constructor(address _parent) ActionBuilder(_parent) {}
 
-  function getActions() external pure override returns (Action[] memory _actions) {
-    _actions = new Action[](0);
-  }
+  function getActions() external pure override returns (Action[] memory _actions) {}
 }
