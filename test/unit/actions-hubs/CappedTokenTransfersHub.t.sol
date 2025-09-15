f7094aad89a7a4912f2182b8d3e9e868412cc4c0
itadaki <itadaki@wonderland.xyz>
2025-09-15 11:05:10 +0200

    chore: reverts License changes

--- test/unit/actions-hubs/CappedTokenTransfersHub.t.sol
+++ test/unit/actions-hubs/CappedTokenTransfersHub.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.29;
 
 import {IOwnerManager} from '@safe-smart-account/interfaces/IOwnerManager.sol';
--- test/unit/mocks/ActionHubForTest.sol
+++ test/unit/mocks/ActionHubForTest.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.29;
 
 import {ActionHub} from 'src/contracts/action-hubs/ActionHub.sol';
