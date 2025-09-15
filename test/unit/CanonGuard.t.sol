8f3a7be3c9035b1b18ec9c081168d1084e63b92d
itadaki <itadaki@wonderland.xyz>
2025-09-15 10:54:37 +0200

    style: removes unused import

--- test/unit/CanonGuard.t.sol
+++ test/unit/CanonGuard.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.29;
 
-import {ActionHubForTest} from './mocks/ActionHubForTest.sol';
 import {CanonGuardForTest} from './mocks/CanonGuardForTest.sol';
 import {IOwnerManager} from '@safe-smart-account/interfaces/IOwnerManager.sol';
 import {ISafe} from '@safe-smart-account/interfaces/ISafe.sol';
