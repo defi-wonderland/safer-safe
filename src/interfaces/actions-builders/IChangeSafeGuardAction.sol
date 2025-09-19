ac1f2fd99586a7b739b9a1816fb56127528516b1
itadaki <itadaki@wonderland.xyz>
2025-09-19 17:28:33 +0200

    feat: improves change safe guard action (#122)
    Closes BES-631

--- src/interfaces/actions-builders/IChangeSafeGuardAction.sol
+++ src/interfaces/actions-builders/IChangeSafeGuardAction.sol
@@ -6,14 +6,6 @@ pragma solidity 0.8.30;
  * @notice Interface for the ChangeSafeGuardAction contract
  */
 interface IChangeSafeGuardAction {
-  // ~~~ STORAGE METHODS ~~~
-
-  /**
-   * @notice Gets the Safe contract address
-   * @return _safe The Safe contract address
-   */
-  function SAFE() external view returns (address _safe);
-
   /**
    * @notice Gets the safe guard contract address
    * @return _safeGuard The safe guard contract address
--- src/interfaces/factories/IChangeSafeGuardActionFactory.sol
+++ src/interfaces/factories/IChangeSafeGuardActionFactory.sol
@@ -10,12 +10,8 @@ interface IChangeSafeGuardActionFactory {
 
   /**
    * @notice Creates a ChangeSafeGuardAction contract
-   * @param _safe The Safe contract address
    * @param _safeGuard The safe guard contract address
    * @return _changeSafeGuardAction The ChangeSafeGuardAction contract address
    */
-  function createChangeSafeGuardAction(
-    address _safe,
-    address _safeGuard
-  ) external returns (address _changeSafeGuardAction);
+  function createChangeSafeGuardAction(address _safeGuard) external returns (address _changeSafeGuardAction);
 }
