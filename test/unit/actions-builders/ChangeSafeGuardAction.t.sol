fa1943a9e7ed28d62eec5693ed5b1422468b2234
itadaki <itadaki@wonderland.xyz>
2025-09-19 16:23:51 +0200

    test: fixes test

--- test/unit/actions-builders/ChangeSafeGuardAction.t.sol
+++ test/unit/actions-builders/ChangeSafeGuardAction.t.sol
@@ -4,27 +4,34 @@ pragma solidity 0.8.30;
 import {IGuardManager} from '@safe-smart-account/interfaces/IGuardManager.sol';
 import {Test} from 'forge-std/Test.sol';
 import {ChangeSafeGuardAction} from 'src/contracts/actions-builders/ChangeSafeGuardAction.sol';
-import {ICanonGuard} from 'src/interfaces/ICanonGuard.sol';
+
+import {ISafeManageable} from 'src/interfaces/ISafeManageable.sol';
 import {IActionsBuilder} from 'src/interfaces/actions-builders/IActionsBuilder.sol';
 
 contract UnitChangeSafeGuardAction is Test {
   ChangeSafeGuardAction public changeSafeGuardAction;
-  address public safeGuard = makeAddr('safeGuard');
+  address public mockSafe = makeAddr('safe');
+  address public mockCanonGuard = makeAddr('canonGuard');
 
   function setUp() external {
-    changeSafeGuardAction = new ChangeSafeGuardAction(address(0), safeGuard);
+    changeSafeGuardAction = new ChangeSafeGuardAction(address(0), mockCanonGuard);
   }
 
   function test_ConstructorWhenCalled() external view {
     // it sets the safe guard address
-    assertEq(changeSafeGuardAction.SAFE_GUARD(), safeGuard);
+    assertEq(changeSafeGuardAction.SAFE_GUARD(), mockCanonGuard);
   }
 
-  function test_GetActionsWhenCalled() external view {
-    // it returns an action to disapprove the actions builder or action hub
+  function test_GetActionsWhenCalled() external {
+    vm.mockCall(mockCanonGuard, abi.encodeWithSelector(ISafeManageable.SAFE.selector), abi.encode(mockSafe));
+    vm.expectCall(mockCanonGuard, abi.encodeWithSelector(ISafeManageable.SAFE.selector));
+
+    // it returns an action to change the safe guard
+    vm.prank(mockCanonGuard);
     IActionsBuilder.Action[] memory actions = changeSafeGuardAction.getActions();
-    assertEq(actions[0].target, address(ICanonGuard(msg.sender).SAFE()));
-    assertEq(actions[0].data, abi.encodeCall(IGuardManager.setGuard, (safeGuard)));
+    assertEq(actions.length, 1);
+    assertEq(actions[0].target, mockSafe);
+    assertEq(actions[0].data, abi.encodeCall(IGuardManager.setGuard, (mockCanonGuard)));
     assertEq(actions[0].value, 0);
   }
 }
