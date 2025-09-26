54637ae0802c949675ec8146ace470c6f4000666
Austrian <114922365+0xAustrian@users.noreply.github.com>
2025-09-26 11:35:10 -0300

    fix: re-order parameters of the event (#127)
    Closes BES-640

--- src/interfaces/ICanonGuard.sol
+++ src/interfaces/ICanonGuard.sol
@@ -40,13 +40,13 @@ interface ICanonGuard is ISafeManageable {
 
   /**
    * @notice Emitted when a transaction is queued
-   * @param _actionHub The actionHub contract address (0 if no actionHub was used)
    * @param _proposer The address of the proposer of the transaction
    * @param _actionsBuilder The actions builder contract address
+   * @param _actionHub The actionHub contract address (0 if no actionHub was used)
    * @param _txIsPreApproved Whether the transaction is pre-approved
    */
   event TransactionQueued(
-    address indexed _actionHub, address indexed _proposer, address indexed _actionsBuilder, bool _txIsPreApproved
+    address indexed _proposer, address indexed _actionsBuilder, address indexed _actionHub, bool _txIsPreApproved
   );
 
   /**
