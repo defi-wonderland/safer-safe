8a159ec829ed2d2bd7f01d1e3319173473662e2f
0xAustrian <114922365+0xAustrian@users.noreply.github.com>
2025-10-24 10:33:46 -0300

    fix: remove cap left from event emission

--- src/contracts/action-hubs/CappedTokenTransfersHub.sol
+++ src/contracts/action-hubs/CappedTokenTransfersHub.sol
@@ -88,7 +88,6 @@ contract CappedTokenTransfersHub is ActionHub, ICappedTokenTransfersHub, SafeMan
   /// @inheritdoc ICappedTokenTransfersHub
   function updateState(address _token, uint256 _amount) external isSafe {
     uint256 _currentEpoch = (block.timestamp - STARTING_TIMESTAMP) / EPOCH_LENGTH;
-    uint256 _tokenCap = cap[_token];
 
     // If we're in a new epoch, reset the spending
     if (_currentEpoch > currentEpoch) {
@@ -98,12 +97,11 @@ contract CappedTokenTransfersHub is ActionHub, ICappedTokenTransfersHub, SafeMan
 
     totalSpent[_token] += _amount;
 
-    if (totalSpent[_token] > _tokenCap) {
+    if (totalSpent[_token] > cap[_token]) {
       revert CapExceeded();
     }
 
-    uint256 _capLeft = _tokenCap - totalSpent[_token];
-    emit StateUpdated(_token, _amount, currentEpoch, _capLeft);
+    emit StateUpdated(_token, _amount, currentEpoch);
   }
 
   /// @inheritdoc ICappedTokenTransfersHub
--- src/interfaces/action-hubs/ICappedTokenTransfersHub.sol
+++ src/interfaces/action-hubs/ICappedTokenTransfersHub.sol
@@ -15,9 +15,8 @@ interface ICappedTokenTransfersHub is ISafeManageable {
    * @param _token The token that was updated
    * @param _amountSpent The amount of tokens that were spent
    * @param _currentEpoch The current epoch after the update
-   * @param _capLeft The cap left for the token in the current epoch
    */
-  event StateUpdated(address indexed _token, uint256 _amountSpent, uint256 _currentEpoch, uint256 _capLeft);
+  event StateUpdated(address indexed _token, uint256 _amountSpent, uint256 _currentEpoch);
 
   // ~~~ ERRORS ~~~
 
