502e85bf7c978630937a0d28877bf927f8372823
0xAustrian <114922365+0xAustrian@users.noreply.github.com>
2025-10-27 09:46:35 -0300

    fix: double inheritance

--- test/unit/mocks/EmergencyModeHookForTest.sol
+++ test/unit/mocks/EmergencyModeHookForTest.sol
@@ -2,14 +2,13 @@
 pragma solidity 0.8.30;
 
 import {EmergencyModeHook} from 'contracts/EmergencyModeHook.sol';
-import {SafeManageable} from 'contracts/SafeManageable.sol';
 
 contract EmergencyModeHookForTest is EmergencyModeHook {
   constructor(
     address _emergencyTrigger,
     address _emergencyCaller,
     address _safe
-  ) EmergencyModeHook(_emergencyTrigger, _emergencyCaller) SafeManageable(_safe) {}
+  ) EmergencyModeHook(_emergencyTrigger, _emergencyCaller, _safe) {}
 
   function forTest_onBeforeExecution() public {
     super._onBeforeExecution();
