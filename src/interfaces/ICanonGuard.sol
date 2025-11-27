fc54129349e6000ecf9e78530772d1faba35ade2
0xAustrian <114922365+0xAustrian@users.noreply.github.com>
2025-11-27 17:05:30 -0300

    feat: improve natspec

--- src/interfaces/ICanonGuard.sol
+++ src/interfaces/ICanonGuard.sol
@@ -176,6 +176,7 @@ interface ICanonGuard is IOnlyCanonGuard, IEmergencyModeHook {
    * @notice Executes a queued transaction using the approved hash signers
    * @dev Can be called by anyone
    * @dev The transaction must have passed its execution delay period, but not its expiry delay period
+   * @dev If the actions to be executed involve sending or using a value, this value will be extracted from the SAFE. If the SAFE does not have sufficient ETH, it may need to be topped up.
    * @param _actionsBuilder The actions builder contract address of the transaction to execute
    */
   function executeTransaction(address _actionsBuilder) external;
@@ -185,6 +186,7 @@ interface ICanonGuard is IOnlyCanonGuard, IEmergencyModeHook {
    * @dev Can be called by anyone
    * @dev The transactions must have passed their execution delay period, but not their expiry delay period
    * @dev Each transaction must have been approved using consecutive SAFE nonces.
+   * @dev If the actions to be executed involve sending or using a value, this value will be extracted from the SAFE. If the SAFE does not have sufficient ETH, it may need to be topped up.
    * @param _actionsBuilders The array of actions builder contract addresses of the transactions to execute
    */
   function executeTransactions(address[] memory _actionsBuilders) external;
